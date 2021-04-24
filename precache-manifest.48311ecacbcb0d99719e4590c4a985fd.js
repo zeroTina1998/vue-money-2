c765ed3387bb1c1598db848019e3aa015f9b6fc9
Tina <1098299287@qq.com>
2021-04-24 16:59:42 +0800

    deploy

---
+++ precache-manifest.48311ecacbcb0d99719e4590c4a985fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258d03798f4b04885aac",
+    "url": "/vue-money-2/css/app.a2c08f7b.css"
+  },
+  {
+    "revision": "0312dc497e114d84ba172cf9933ec7b4",
+    "url": "/vue-money-2/index.html"
+  },
+  {
+    "revision": "258d03798f4b04885aac",
+    "url": "/vue-money-2/js/app.6fec0392.js"
+  },
+  {
+    "revision": "3bd3471ba608d1726eb7",
+    "url": "/vue-money-2/js/chunk-vendors.9eb9416f.js"
+  },
+  {
+    "revision": "ebd2e529c54677604846e03676baa9ad",
+    "url": "/vue-money-2/manifest.json"
+  },
+  {
+    "revision": "36443d701035e5d95edad73b26a21b14",
+    "url": "/vue-money-2/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-money-2/robots.txt"
+  }
+]);
